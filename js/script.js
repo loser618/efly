15357f675ab12fe9f7ca90076ef9e27c85aa36ab
loser618 <s763238671@163.com>
2016-12-03 21:19:53 +0800

    homework

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+/*var beyond = {
+	functionIn:'1983',
+	founded:'香港',
+	artist: ['黄家驹','黄家强','黄贯中','叶世荣']
+	};
+	
+	beyond.showArtist = function () {
+		for (var i = 0; i <this.artist.length; i++) {
+			document.write(this.artist[i]);
+		}
+	};
+		beyond.showArtist();
+		
+		var property;
+		for (property in beyond) {
+			if (typeof beyond[property] !== 'function') {
+				console.log(beyond[property]);
+			}
+		}
+	
+	   console.log(beyond);*/
+
